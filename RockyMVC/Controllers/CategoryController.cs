2884228bfa0cc5bdb7fa31ab12d5715f6d9f1c2d
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-11-25 17:55:58 -0400

    Import Category db to Category controller

---
+++ RockyMVC/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RockyMVC.Data;
+using RockyMVC.Models;
+
+namespace RockyMVC.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			IEnumerable<Category> objList = _db.Categories;
+			return View(objList);
+		}
+	}
+}
