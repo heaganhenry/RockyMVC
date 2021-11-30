cf552903ba67ba1d7240239e69c3f3567869d987
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-11-30 11:17:41 -0400

    Add validation tags

--- RockyMVC/Models/Category.cs
+++ RockyMVC/Models/Category.cs
@@ -12,8 +12,11 @@ namespace RockyMVC.Models
 		[Key]
 		public int Id { get; set; }
 
+		[Required]
 		public string Name { get; set; }
 
+		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
 		[DisplayName("Display Order")]
 		public int DisplayOrder { get; set; }
 	}
