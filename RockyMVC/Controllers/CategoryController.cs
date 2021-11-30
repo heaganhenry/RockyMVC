99fa3e993ec74cd816bb1276da64c5a86ebcdece
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-11-30 11:19:09 -0400

    Improve POST method

--- RockyMVC/Controllers/CategoryController.cs
+++ RockyMVC/Controllers/CategoryController.cs
@@ -34,9 +34,13 @@ namespace RockyMVC.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Create(Category obj)
 		{
-			_db.Categories.Add(obj);
-			_db.SaveChanges();
-			return RedirectToAction("Index");
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
 		}
 	}
 }
