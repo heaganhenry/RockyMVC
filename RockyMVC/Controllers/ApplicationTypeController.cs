4b2c3fedf63aaa378c638b5af0c79819fd18c469
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-12-05 17:41:40 -0400

    Complete application

--- RockyMVC/Controllers/ApplicationTypeController.cs
+++ RockyMVC/Controllers/ApplicationTypeController.cs
@@ -23,18 +23,72 @@ namespace RockyMVC.Controllers
 			return View(objList);
 		}
 
-		//GET - Create
+		//GET - CREATE
 		public IActionResult Create()
 		{
 			return View();
 		}
 
-		//POST - Create
+		//POST - CREATE
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public IActionResult Create(ApplicationType obj)
 		{
-			_db.ApplicationTypes.Add(obj);
+			if (ModelState.IsValid)
+			{
+				_db.ApplicationTypes.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			
+			return View(obj);
+		}
+
+		//GET - EDIT
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+				return NotFound();
+
+			var obj = _db.ApplicationTypes.Find(id);
+			return View(obj);
+		}
+
+		//POST - EDIT
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(ApplicationType obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.ApplicationTypes.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(obj);
+		}
+
+		//GET - DELETE
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+				return NotFound();
+
+			var obj = _db.ApplicationTypes.Find(id);
+			return View(obj);
+		}
+
+		//POST - DELETE
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePost(int? id)
+		{
+			var obj = _db.ApplicationTypes.Find(id);
+			if (obj == null)
+				return NotFound();
+
+			_db.ApplicationTypes.Remove(obj);
 			_db.SaveChanges();
 			return RedirectToAction("Index");
 		}
